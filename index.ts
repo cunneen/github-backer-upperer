88619163ce094d3dd18cd07cc764e9db6dce0e24
Mike Cunneen <mike.cunneen@elastik.com>
2024-02-17 01:16:15 +0800

    add .env.vault

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Octokit, App } from "octokit";
 import 'dotenv/config'
 
-const app = new App({ appId, privateKey });
+const app = new App({ process.env.GITHUB_APP_ID, process.env.GITHUB_APP_PRIVATE_KEY });
 const { data: slug } = await app.octokit.rest.apps.getAuthenticated();
 const octokit = await app.getInstallationOctokit(123);
 await octokit.rest.issues.create({
